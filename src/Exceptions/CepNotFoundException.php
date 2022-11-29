c9b69a37b2c4c560627802ec40d457ba97054b91
Lucas Nepomuceno <lsn.nepomuceno@gmail.com>
2022-11-29 11:22:18 -0300

    feature: adds cep not found exception

--- src/Exceptions/CepNotFoundException.php
+++ src/Exceptions/CepNotFoundException.php
@@ -3,8 +3,18 @@
 namespace LSNepomuceno\LaravelBrazilianCeps\Exceptions;
 
 use Exception;
+use Stringable;
 
-class CepNotFoundException extends Exception
+class CepNotFoundException extends Exception implements Stringable
 {
+    public function __construct(string|int $cep, int $code = 0, Exception $previous = null)
+    {
+        $message = "We were unable to locate the CEP: \"{$cep}\" on any of the available providers.";
+        parent::__construct($message, $code, $previous);
+    }
 
+    public function __toString(): string
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
 }
