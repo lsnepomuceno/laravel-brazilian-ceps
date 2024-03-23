977a0349ae4a7a31710f23c485354647816f3bb0
Lucas Nepomuceno <lsn.nepomuceno@gmail.com>
2024-03-23 16:03:33 -0300

    feat: adds getBaseUrl method

--- src/Contracts/ConsultableCEPProvider.php
+++ src/Contracts/ConsultableCEPProvider.php
@@ -7,4 +7,6 @@ use LSNepomuceno\LaravelBrazilianCeps\Entities\CepEntity;
 interface ConsultableCEPProvider
 {
     public function get(string $cep): ?CepEntity;
+
+    public function getBaseUrl(): string;
 }
